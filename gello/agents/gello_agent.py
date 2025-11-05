58a44caba457d7b0291a331ff84293d9fbebfddd
r-muthu <muthuramaswamy2003@gmail.com>
2025-11-05 15:41:11 +0800

    added port

--- gello/agents/gello_agent.py
+++ gello/agents/gello_agent.py
@@ -113,12 +113,25 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
             1 * np.pi / 2,
             3 * np.pi / 2,
             2 * np.pi / 2,
-            -2 * np.pi / 2,
+            2 * np.pi / 2,
             4 * np.pi / 2,
-            -2 * np.pi / 2,
+            1 * np.pi / 2,
         ),
         joint_signs=(1, 1, -1, 1, 1, 1), 
         gripper_config=(7, 282, 240),  # Reversed: now starts open (-30) and closes on press (24) 
+    ),
+    "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT6Z5I37-if00-port0": DynamixelRobotConfig(
+        joint_ids=(1, 2, 3, 4, 5, 6),
+        joint_offsets=(
+            2 * np.pi / 2, #offsets to address delta between master and slave
+            3 * np.pi / 2,
+            2 * np.pi / 2,
+            2 * np.pi / 2,
+            4 * np.pi / 2,
+            5 * np.pi / 2,
+        ),
+        joint_signs=(1, 1, -1, 1, 1, 1), 
+        gripper_config=(7, 193, 240),  # Reversed: now starts open (-30) and closes on press (24) 
     ),
 }
 
